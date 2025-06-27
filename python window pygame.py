b8a4b5aabdfd5812cb059d7cc5edbbc5c0124853
AdiotChezBurga <amuah7109@icloud.com>
2025-06-27 10:18:47 -0400

    python pygame window

--- python window pygame.py
+++ python window pygame.py
@@ -5,20 +5,6 @@ screen = pygame.display.set_mode((800, 600))
 pygame.display.set_caption("Hello Pygame")
 clock = pygame.time.Clock()
 
-class Sprite:
-    def __init__(self,img:pygame.Surface,pos:tuple = (400,300), color:tuple = (0, 0, 255)):
-        super().__init__()
-        self.pos = pos
-        self.color = color
-        self.rotate = 0
-        self.base_image = img
-        self.size = 1
-    
-    def draw(self, surface:pygame.Surface):
-        image = pygame.transform.rotozoom(self.base_image, self.rotate, self.size)
-        rect = image.get_rect(center=self.pos)
-        surface.blit(image, rect.topleft)
-
 
 def main():
     while True:
