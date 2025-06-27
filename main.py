2dcd7be2c22de44daa1d806744e2f89ddaef6eba
aidenscoder <amuah7109@icloud.com>
2025-06-27 09:11:11 -0400

    commit changes

--- main.py
+++ main.py
@@ -5,6 +5,21 @@ screen = pygame.display.set_mode((800, 600))
 pygame.display.set_caption("Hello Pygame")
 clock = pygame.time.Clock()
 
+class Sprite:
+    def __init__(self,img:pygame.Surface,pos:tuple = (400,300), color:tuple = (0, 0, 255)):
+        super().__init__()
+        self.pos = pos
+        self.color = color
+        self.rotate = 0
+        self.base_image = img
+        self.size = 1
+    
+    def draw(self, surface:pygame.Surface):
+        image = pygame.transform.rotozoom(self.base_image, self.rotate, self.size)
+        rect = image.get_rect(center=self.pos)
+        surface.blit(image, rect.topleft)
+
+
 def main():
     while True:
         for event in pygame.event.get():
@@ -12,6 +27,7 @@ def main():
                 pygame.quit()
                 sys.exit()
 
+        
         screen.fill((255, 255, 255)) 
         pygame.display.flip()    
         clock.tick(60)         
