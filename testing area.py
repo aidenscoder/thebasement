94d6e99c1c8f0e08dd3f889c7b35727fd2f274ca
AdiotChezBurga <amuah7109@icloud.com>
2025-07-01 08:18:39 -0400

    updated

--- testing area.py
+++ testing area.py
@@ -3,6 +3,7 @@ def kwarg_dict(**kwargs):
 
 class Storage:
     def __init__(self,**kwargs):
+        self._name = ""
         for key,value in kwargs.items():
             setattr(self,key,value)
             
@@ -20,33 +21,47 @@ class Storage:
     
     @property
     def name(self):
-        self_id = id(self)
-        for key,val in globals().items():
-            if self_id == id(val):
-                return key.replace("'","")
-            else: continue
+        if self._name == "":
+            for key,value in globals().items():
+                if self.id == id(value):
+                    self._name = key.replace("'","")
+                    return self._name
+                else: 
+                    continue   
+        else:
+            return self._name  
             
 class FakeGeneric:
     def __init__(self):
         pass
     
     def __getitem__(self,passed:type):
-        def wrapper(val):
-            if isinstance(val,passed):
-                class _(passed):
+        def wrapper(value):
+            if isinstance(value,passed):
+                class new_object(passed):
+                    
                     def __new__(cls,val:object):
+                        cls._name = ""
                         return super().__new__(cls,val)
                     
-                    def __init__(self):
-                        self.type = passed.__name__.replace("'","")
                     
+                    def operate(self,val:object):
+                        for key,value in globals().items():
+                            if self.id == id(value):
+                                globals()[key] = new_object(val)
+                                break
+                        
                     @property
                     def name(self):
-                        self_id = id(self)
-                        for key,val in globals().items():
-                            if self_id == id(val):
-                                return key.replace("'","")
-                            else: continue
+                        if self._name == "":
+                            for key,value in globals().items():
+                                if self.id == id(value):
+                                    self._name = key.replace("'","")
+                                    return self._name
+                                else: 
+                                    continue   
+                        else:
+                            return self._name                 
                             
                     @property  
                     def id(self):
@@ -55,13 +70,13 @@ class FakeGeneric:
                     @property
                     def dict_view(self):
                         return self.__dict__
-                    
-  
+              
+                return new_object(value)
             else:
-                message = f"Expected type, {passed.__name__} but got {type(val).__name__}."
+                message = f"Expected type, {passed.__name__} but got {type(value).__name__}."
                 raise TypeError(message)
         return wrapper
 
-var = FakeGeneric()
+new = FakeGeneric()
 del FakeGeneric
-x = var[int](10)
+
